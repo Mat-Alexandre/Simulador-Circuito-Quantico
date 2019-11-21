c75ff69266b39cf865823da61f169e1de13cb31d
Matheus Alexandre <matheusasj@live.com>
2019-11-21 08:12:45 -0300

    Implementação da medição

--- main.cu
+++ main.cu
@@ -1,57 +1,53 @@
 #include <stdio.h>
 #include "quantum.cuh"
 
-#define N 4
-#define T 2
-
 int main() {
+	srand(time(NULL));
+	// Tamanhos de estruturas
 	int qbit_size = N * sizeof(qubit);
 	int vect_size = T * sizeof(int);
 	
+	// Variáveis iniciais
 	qubit q[N], *d_q;
+	int mesure[N], *d_mesure;
+	int teste[1], *d_teste;
 	
 	// Valores para teste
+	q[0].amplitude[0].real = 1.0f;
+	q[0].amplitude[0].imag = 0.0f;
 	q[0].amplitude[1].real = 0.0f;
-	q[1].amplitude[1].real = 0.0f;
-	q[2].amplitude[0].real = 0.0f;
-	q[3].amplitude[0].real = 0.0f;
+	q[0].amplitude[1].imag = 0.0f;
 
 	// Vetores alvo e controles
-	int t[T] = { 0, 1 }, c1[T] = { 2, 3 }, c2[T];
-	int *d_t, *d_c1, *d_c2;
+	int t[T] = { 0, 1 }, c1[T] = { 2, 3 };
+	int *d_t, *d_c1;
 
 	// Alocação dos vetores Devices
 	cudaMalloc((void**) &d_q, qbit_size);
-	cudaMalloc((void**) &d_t, vect_size);
-	cudaMalloc((void**)&d_c1, vect_size);
+	cudaMalloc((void**) &d_mesure, N*sizeof(int));
 
 	// Cópia para as variáveis Devices
 	cudaMemcpy( d_q,  q, qbit_size, cudaMemcpyHostToDevice);
-	cudaMemcpy( d_t,  t, vect_size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_c1, c1, vect_size, cudaMemcpyHostToDevice);
 	
 	// Imprimindo os resultados iniciais
-	printf("-------INICIO-------\n");
-	printQubit(q, N);
-	printf("Qubit alvo: %d\n", t[0]);
-	printf("Qubit controle: %d\n", c1[0]);
-
+	printf("--AMPLITUDE INICIAL--\n");
+	printQubit(q, NULL, N);
+	
 	// Aplicação da(s) porta(s)
-	cnotGate<<<1, N>>>(d_q, d_t, d_c1);
+	hadamardGate<<<B, N >>>(d_q);
+	mesureQubit<<<B, N>>>(d_q, d_mesure, rand() % RAND_PRECISION);
 
 	// Cópia para as variáveis Host
 	cudaMemcpy( q, d_q,  qbit_size, cudaMemcpyDeviceToHost);
-	cudaMemcpy( t, d_t,  vect_size, cudaMemcpyDeviceToHost);
-	cudaMemcpy(c1, d_c1, vect_size, cudaMemcpyDeviceToHost);
-
+	cudaMemcpy( mesure,   d_mesure, N*sizeof(int), cudaMemcpyDeviceToHost);
+	
 	// Imprimindo os resultados
-	printf("------RESULTADO-----\n");
-	printQubit(q, N);
+	printf("---AMPLITUDE FINAL---\n");
+	printQubit(q, mesure, N);
 	
 	// Liberando as variáveis alocadas
 	cudaFree(d_q);
-	cudaFree(d_t);
-	cudaFree(d_c1);
+	cudaFree(d_mesure);
 
 	return 0;
 }
