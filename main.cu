286250daed8233e030443bc628aeb167ac1571bc
Matheus Alexandre <matheusasj@live.com>
2019-12-19 07:43:53 -0300

    Alteração da estrutura da main

--- main.cu
+++ main.cu
@@ -1,57 +1,31 @@
 #include <stdio.h>
+#include <windows.h>
 #include "quantum.cuh"
 
 int main() {
+	// Vari�veis iniciais
 	srand(time(NULL));
-	// Tamanhos de estruturas
-	int qbit_size = N * sizeof(qubit);
-	int vect_size = T * sizeof(int);
-	
-	// Variáveis iniciais
 	float rand_value = rand() % RAND_PRECISION;
 	float percentage = (float)(rand_value) / RAND_PRECISION;
-	qubit q[N], *d_q;
-	int mesure[N], *d_mesure;
-	int teste[1], *d_teste;
+	int quantidade;
+
+	printf("Quantidade de qbits> \n");
+	scanf("%d", &quantidade);
+
+	simulator simu = initSimulator(quantidade);
+	simulator d_simu = initSimulatorDevice(quantidade);
+
+	// Execução
+	cpyToDevice(simu, d_simu);
 	
-	// Valores para teste
-	q[0].amplitude[0].real = 1.0f;
-	q[0].amplitude[0].imag = 0.0f;
-	q[0].amplitude[1].real = 0.0f;
-	q[0].amplitude[1].imag = 0.0f;
-
-	// Vetores alvo e controles
-	int t[T] = { 0, 1 }, c1[T] = { 2, 3 };
-	int *d_t, *d_c1;
-
-	// Alocação dos vetores Devices
-	cudaMalloc((void**) &d_q, qbit_size);
-	cudaMalloc((void**) &d_mesure, N*sizeof(int));
-
-	// Cópia para as variáveis Devices
-	cudaMemcpy( d_q,  q, qbit_size, cudaMemcpyHostToDevice);
+	hadamardGate<<<1, quantidade>>>(d_simu.q);
+	mesureQubit<<<1, quantidade>>> (d_simu.q , d_simu.mesure, percentage);
 	
-	// Imprimindo os resultados iniciais
-	printf("--AMPLITUDE INICIAL--\n");
-	printQubit(q, NULL, N);
-	
-	// Aplicação da(s) porta(s)
-	hadamardGate<<<B, N >>>(d_q);
-	notGate<<<B, N>>>(d_q);
-	mesureQubit<<<1, N>>>(d_q, d_mesure, percentage);
+	cpyToHost(d_simu, simu);
 
-	// Cópia para as variáveis Host
-	cudaMemcpy( q, d_q,  qbit_size, cudaMemcpyDeviceToHost);
-	cudaMemcpy( mesure,   d_mesure, N*sizeof(int), cudaMemcpyDeviceToHost);
+	printQubit(simu.q, simu.mesure, simu.size);
 
-	// Imprimindo os resultados
-	printf("Valor sorteado: %f\n", percentage);
-	printf("---AMPLITUDE FINAL---\n");
-	printQubit(q, mesure, N);
-	
-	// Liberando as variáveis alocadas
-	cudaFree(d_q);
-	cudaFree(d_mesure);
-
+	freeSimulatorDevice(d_simu);
+	freeSimulatorHost(simu);
 	return 0;
 }
