58b13049692835b20852cd8075729566762c4ced
Matheus Alexandre <matheusasj@live.com>
2020-02-27 12:08:48 -0300

    Aplicação do Algoritmo de Grover

--- main.cu
+++ main.cu
@@ -3,27 +3,59 @@
 #include "quantum.cuh"
 
 int main() {
-	// Vari�veis iniciais
+	// Variáveis iniciais
 	srand(time(NULL));
 	float rand_value = rand() % RAND_PRECISION;
 	float percentage = (float)(rand_value) / RAND_PRECISION;
-	int quantidade;
+	int quantidade = 2;
 
-	printf("Quantidade de qbits> \n");
-	scanf("%d", &quantidade);
-
-	simulator simu = initSimulator(quantidade);
+	simulator simu = initSimulatorHost(quantidade);
 	simulator d_simu = initSimulatorDevice(quantidade);
 
-	// Execução
+	printQubit(simu, 0); // Visualização dos estados iniciais
+
+	// Copiando valores para variável no device
 	cpyToDevice(simu, d_simu);
 	
-	hadamardGate<<<1, quantidade>>>(d_simu.q);
+	// Execução do Algoritmo de Grover
+	// Exemplo obtido através do vídeo https://youtu.be/Uw6zEMSxKvg por daytonellwanger
+
+	hadamardGate_All<<<1, quantidade>>>(d_simu);
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+	cnotGate<<<1, quantidade>>>(d_simu.q, {1}, {0});
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+	hadamardGate_All<<<1, quantidade>>>(d_simu);
+
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+	cnotGate<<<1, quantidade>>>(d_simu.q, {1}, {0});
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+
+	notGate<<<1, quantidade>>>(d_simu.q[0]);
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+	cnotGate<<<1, quantidade>>>(d_simu.q, {1}, {0});
+	notGate<<<1, quantidade>>>(d_simu.q[0]);
+	hadamardGate<<<1, quantidade>>>(d_simu.q[1]);
+
+	hadamardGate<<<1, quantidade>>>(d_simu.q[0]);
+	notGate<<<1, quantidade>>>(d_simu.q[1]);
+	cnotGate<<<1, quantidade>>>(d_simu.q, {0}, {1});
+	hadamardGate<<<1, quantidade>>>(d_simu.q[0]);
+	notGate<<<1, quantidade>>>(d_simu.q[1]);
+
+	hadamardGate_All<<<1, quantidade>>>(d_simu);
+
+
+	// Medindo qbits
 	mesureQubit<<<1, quantidade>>> (d_simu.q , d_simu.mesure, percentage);
 	
+	// Copiando valores para variável no kernel
 	cpyToHost(d_simu, simu);
 
-	printQubit(simu.q, simu.mesure, simu.size);
+	// Exibindo resultados
+	printQubit(simu, 1);
+	/*
+	Resultado esperado: 100% -- 10
+	*/
 
 	freeSimulatorDevice(d_simu);
 	freeSimulatorHost(simu);
